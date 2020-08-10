50bee511ffcc67410c1c91b5c3747aa229b54a0e
Nikolay Marusov <nmarusov@gmail.com>
2020-08-10 16:26:25 +0300

    Заменён элемент ввода основного пользователя.

--- frontend/src/components/Tabs.vue
+++ frontend/src/components/Tabs.vue
@@ -1,43 +1,26 @@
 <template>
   <div id="form">
-    <b-row>
-      <b-col sm="2">
-        <label class="mt-3 mb-3" size="sm">Пользователь:</label>
-      </b-col>
-      <b-col sm="10">
-        <b-form-input
-          class="mt-3 mb-3"
-          size="sm"
-          type="search"
-          v-model="filterCriteria"
-          v-on:click="toggleDropDown()"
-          v-on:keyup.enter="selectItem()"
-          placeholder="Введите имя пользователя"
-        ></b-form-input>
-        <b-collapse id="drop-down">
-          <b-table
-            no-border-collapse
-            ref="collapsibleTable"
-            responsive="sm"
-            selectable
-            select-mode="single"
-            sticky-header="200px"
-            thead-class="d-none"
-            v-model="filteredRows"
-            :fields="selectFields"
-            :filter="filterCriteria"
-            :items="users"
-            @row-selected="dropdownRowSelected"
-          ></b-table>
-        </b-collapse>
-      </b-col>
-    </b-row>
+    <b-form-group
+      id="fieldset-horizontal"
+      label-cols-sm="auto"
+      label-cols-lg="auto"
+      label="Пользователь:"
+      label-for="user-selector"
+    >
+      <multiselect
+        v-model="user"
+        :options="users"
+        placeholder="Начните вводить имя пользователя"
+        label="name"
+        track-by="name"
+        @input="updateTables()"
+      ></multiselect>
+    </b-form-group>
     <b-tabs content-class="mt-3" v-model="tabIndex">
       <b-tab title="Чьи обязанности исполняет" active>
         <b-table
           :fields="substitutedFields"
           :items="substituted"
-          :keyword="userName"
           selectable
           select-mode="multi"
           @row-selected="onRowSelected"
@@ -63,16 +46,12 @@
         <b-button
           class="form-button"
           v-b-modal.adding-substituted
-          :disabled="userId == undefined"
+          :disabled="user == undefined"
           >Добавить</b-button
         >
       </b-tab>
       <b-tab title="Кто исполняет обязанности пользователя">
-        <b-table
-          :fields="deputiesFields"
-          :items="deputies"
-          :keyword="userName"
-        ></b-table>
+        <b-table :fields="deputiesFields" :items="deputies"></b-table>
       </b-tab>
     </b-tabs>
     <b-modal
@@ -155,8 +134,7 @@ export default {
   data() {
     return {
       tabIndex: 0,
-      userName: "",
-      userId: undefined,
+      user: undefined,
       substituted: [],
       selectedSubstituted: [],
       deputies: [],
@@ -218,7 +196,7 @@ export default {
       });
 
       client
-        .post(`/users/substituted/${this.userId}/add`, this.addedUsers)
+        .post(`/users/substituted/${this.user.id}/add`, this.addedUsers)
         .then((response) => {
           if (response.status != 200) {
             return;
@@ -242,7 +220,7 @@ export default {
       // Получить соответствующий список пользователей
       // для указанного пользователя
       client
-        .get(`/users/substituted/${this.userId}`)
+        .get(`/users/substituted/${this.user.id}`)
         .then((response) => {
           this.substituted = response.data.users;
         })
@@ -252,7 +230,7 @@ export default {
     },
     updateDeputies() {
       client
-        .get(`/users/deputies/${this.userId}`)
+        .get(`/users/deputies/${this.user.id}`)
         .then((response) => {
           this.deputies = response.data.users;
         })
@@ -264,24 +242,6 @@ export default {
       this.updateSubstituted();
       this.updateDeputies();
     },
-    toggleDropDown() {
-      this.$root.$emit("bv::toggle::collapse", "drop-down");
-    },
-    selectItem() {
-      if (this.filteredRows.length === 1) {
-        this.$refs.collapsibleTable.selectRow(0);
-      }
-    },
-    dropdownRowSelected(rowArray) {
-      // No rows or 1 row can be selected
-      if (rowArray.length === 1) {
-        this.$emit("item-selected", rowArray[0]);
-        this.filterCriteria = rowArray[0].name;
-        this.toggleDropDown();
-        this.userId = rowArray[0].id;
-        this.updateTables();
-      }
-    },
     removeSubstituted() {
       let users = [];
 
@@ -290,7 +250,7 @@ export default {
       });
 
       client
-        .post(`/users/substituted/${this.userId}/remove`, users)
+        .post(`/users/substituted/${this.user.id}/remove`, users)
         .then((response) => {
           if (response.status != 200) {
             return;
