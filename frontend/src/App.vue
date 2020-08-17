70d63e5da38b69cf845c10ec74b24b603aeb9fc6
Nikolay Marusov <nmarusov@gmail.com>
2020-08-17 14:46:52 +0300

    Автоматическое добавление токена в заголовки запросов.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,9 +6,22 @@
 <script>
 import axios from "axios";
 
-export const HTTP = axios.create({
+let HTTP = axios.create({
   baseURL: "http://10.254.63.19:1402",
 });
+
+HTTP.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("jwt");
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export { HTTP };
 
 export default {
   components: {},
